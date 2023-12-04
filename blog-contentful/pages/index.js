a4a7f252e90cbc06eaa5858fb26658444e733d8a
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-12-04 15:22:56 -0500

    Basic setup

---
+++ blog-contentful/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Home page</h1>
+    </div>
+  );
+};
+
+export default Homepage;
